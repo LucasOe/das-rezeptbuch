e1464aae73b0a160fe460ba8630f1ee3907bff08
Lucas Oelker <lucas.oelker@gmx.de>
2021-06-02 21:02:57 +0200

    Rework fxml

--- src/application/fxml/KochbuchController.java
+++ src/application/fxml/KochbuchController.java
@@ -2,15 +2,18 @@ package application.fxml;
 
 import application.*;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
-
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.ListView;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 
 public class KochbuchController {
 
@@ -21,19 +24,46 @@ public class KochbuchController {
     private URL location;
 
 	@FXML
-    private ListView rezeptTable;
+	private VBox rezeptListView;
 
     @FXML
     void initialize() {
 		Main.connect();
 		ArrayList<Rezept> rezeptList = Main.getRezeptList();
 
-		ObservableList<String> observableList = FXCollections.observableArrayList();
+		HBox hbox = null;
+		for (int i = 0; i < rezeptList.size(); i++) {
+			if(i % 2 == 0) {
+				// create hbox
+				hbox = new HBox();
 
-		for (Rezept rezept : rezeptList) {
-			observableList.add(rezept.getName());
+				hbox.setSpacing(20);
+				hbox.setAlignment(Pos.TOP_CENTER);
+				hbox.setPrefWidth(1820);
+
+				rezeptListView.getChildren().add(hbox);
+
+				// add Rezept
+				addRezeptPane(hbox, rezeptList.get(i));
+			} else {
+				if( hbox != null) {
+					// add Rezept
+					addRezeptPane(hbox, rezeptList.get(i));
+				}
+			}
 		}
-
-		rezeptTable.setItems(observableList);
     }
+
+	private void addRezeptPane(HBox hbox, Rezept rezept) {
+		try {
+			Pane rezeptPane = FXMLLoader.load(getClass().getResource("rezept.fxml"));
+
+			Label rezeptLabelName = (Label) rezeptPane.lookup("#rezeptLabelName");
+			rezeptLabelName.setText(rezept.getName());
+
+			hbox.getChildren().add(rezeptPane);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
