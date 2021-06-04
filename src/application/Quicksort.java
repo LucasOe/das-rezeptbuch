2592b93b42677000dc1035dba2e02a5df263f35d
Lucas Oelker <lucas.oelker@gmx.de>
2021-06-04 20:04:15 +0200

    Fix quicksort for names

--- src/application/Quicksort.java
+++ src/application/Quicksort.java
@@ -4,22 +4,18 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Quicksort {
-	private static int rezeptToInt(Rezept rezept) {
-		return Character.getNumericValue(rezept.getName().toCharArray()[0]);
-	}
-
 	// https://www.geeksforgeeks.org/java-program-for-quicksort/
 	private static int partition(int sortMode, ArrayList<Rezept> array, int low, int high) {
-		int pivot = rezeptToInt(array.get(high));
+		Rezept pivot = array.get(high);
 		int i = low-1;
 		for(int j=low; j < high; j++) {
 			// sort mode ascending
-			if(sortMode == 1 && rezeptToInt(array.get(j)) <= pivot) {
+			if(sortMode == 1 && array.get(j).getName().compareToIgnoreCase(pivot.getName()) <= 0) {
 				i++;
 				Collections.swap(array, i, j); // swap array[i] and array[j]
 			}
 			// sort mode descending
-			if(sortMode == 2 && rezeptToInt(array.get(j)) >= pivot) {
+			if(sortMode == 2 && array.get(j).getName().compareToIgnoreCase(pivot.getName()) >= 0) {
 				i++;
 				Collections.swap(array, i, j); // swap array[i] and array[j]
 			}
