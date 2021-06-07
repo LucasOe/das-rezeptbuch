b6bb5811da91ff4f09dc9c9b9bd59f938317440e
Lucas Oelker <lucas.oelker@gmx.de>
2021-06-07 17:01:22 +0200

    add clip rectangle to image

--- src/application/fxml/RecipeController.java
+++ src/application/fxml/RecipeController.java
@@ -4,8 +4,10 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.shape.Rectangle;
 
 public class RecipeController {
 
@@ -18,9 +20,12 @@ public class RecipeController {
     @FXML
     private Label recipeLabelName;
 
+	@FXML
+	private ImageView recipeImageView;
+
     @FXML
     void initialize() {
-
+		clipImage();
     }
 
     @FXML
@@ -30,4 +35,15 @@ public class RecipeController {
 
 		System.out.println("Click:" + recipeId);
     }
+
+	private void clipImage() {
+		// create clip rectangle
+		Rectangle clip = new Rectangle(
+			recipeImageView.getFitWidth(), recipeImageView.getFitHeight()
+		);
+		clip.setArcWidth(30);
+		clip.setArcHeight(30);
+		// clip image to rectangle
+		recipeImageView.setClip(clip);
+	}
 }
