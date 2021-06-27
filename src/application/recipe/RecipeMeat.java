ecebf9fbc0df683be5d60a164999d45ff7c4211d
Lucas Oelker <lucas.oelker@gmx.de>
2021-06-27 15:16:43 +0200

    Move stylesheets

--- src/application/recipe/RecipeMeat.java
+++ src/application/recipe/RecipeMeat.java
@@ -14,6 +14,6 @@ public class RecipeMeat extends Recipe {
 
 	@Override
 	public String getStylesheet() {
-		return "application/fxml/stylesheetMeat.css";
+		return "application/fxml/stylesheets/stylesheetMeat.css";
 	}
 }
