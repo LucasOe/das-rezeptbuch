ca39289730f3ee759c50b1b52f089596af8a8963
Lucas Oelker <lucas.oelker@gmx.de>
2021-06-27 16:16:40 +0200

    Add IngredientPanel

--- src/application/fxml/RecipeInfoController.java
+++ src/application/fxml/RecipeInfoController.java
@@ -1,14 +1,18 @@
 package application.fxml;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import application.Main;
+import application.Ingredient;
 import application.recipe.Recipe;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 public class RecipeInfoController {
 
@@ -30,16 +34,35 @@ public class RecipeInfoController {
 	@FXML
 	private ImageView recipeImageFavorite;
 
+	@FXML
+	private VBox ingredientListView;
+
 	private Recipe recipe;
 
     @FXML
     void initialize() {
         recipeLabelName.setText(recipe.getName());
-		// replace line breaks
-		String desc = recipe.getDesc().replace("\\n", "\n\n");
+
+		String desc = recipe.getDesc().replace("\\n", "\n\n"); // replace line breaks
 		recipeLabelDescription.setText(desc);
 
-		Main.debugPrintRecipe(recipe);
+		// add ingredients
+		for(Ingredient ingredient : recipe.getIngredientList()) {
+			try {
+				FXMLLoader loader = new FXMLLoader(getClass().getResource("views/ingredientPaneView.fxml"));
+				//RecipePanelController recipeController = new RecipePanelController(ingredient);
+				//loader.setController(recipeController);
+				HBox ingredientPane = loader.load();
+
+				System.out.println(ingredientPane.getWidth());
+				System.out.println(ingredientPane.getMaxWidth());
+				System.out.println(ingredientPane.getPrefWidth());
+
+				ingredientListView.getChildren().add(ingredientPane);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
     }
 
 	public RecipeInfoController(Recipe recipe) {
